e1ef9d35306a1e62beda5f5efab52134ed1eeb73
ramazankalin <ramazankalin73@gmail.com>
2024-02-07 01:46:37 +0300

    add project files
    MaskeTakip app

---
+++ Business/Concrete/ForeignerManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ForeignerManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            return true;
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,13 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System.Globalization;
+
+Person person1 = new Person();
+person1.FirstName = "Ramazan";
+person1.LastName = "Kalin";
+person1.DateOfBirthYear = 2002;
+person1.NationalIdentity = 12345678910;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
